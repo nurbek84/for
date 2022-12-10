461c7f15888b6f46af3d8967dd8d446d252042c0
nurbek84 <nurbekkojjmberdiev84@gmail.com>
2022-12-10 13:53:09 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                for (int k = 0; k < 5; k++) {
+                s+=1;
+                }
+            }
+        }
+        System.out.println(s);
+    }
+}
+
